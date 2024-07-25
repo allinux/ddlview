020b25ef6920f29ea7f5cb25a814fdfad1722e9b
younghun1.jung <younghun1.jung@lgepartner.com>
2024-07-25 11:27:17 +0000

    .

--- ddlviewer/src/util/mod.rs
+++ ddlviewer/src/util/mod.rs
@@ -6,7 +6,6 @@ pub fn get_cloud_option<P: AsRef<str>>(cred: Credentials, region: P) -> Option<C
     let o = CloudOptions::default().with_aws([
         (AmazonS3ConfigKey::DefaultRegion, "ap-northeast-2".to_string()),
         (AmazonS3ConfigKey::Region, region.as_ref().to_owned()),
-        //(AmazonS3ConfigKey::Bucket, "s3-an2-op-datalake-datatransfer-ha".to_string()),
         (AmazonS3ConfigKey::AccessKeyId, cred.access_key_id().to_owned()),
         (AmazonS3ConfigKey::SecretAccessKey, cred.secret_access_key().to_owned()),
     ]);
